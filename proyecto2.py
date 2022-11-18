3073588a0a5b56f6e9f9d9b9bc22e7e90568f715
storres21 <88690048+storres21@users.noreply.github.com>
2022-11-17 20:18:24 -0500

    hola

--- proyecto2.py
+++ proyecto2.py
@@ -1,12 +1,38 @@
-# {a: [b, c]} means a is before b and c
+"""
+Nombre: Sofía Torres Ramírez.
+ Código: 202014872
+
+ Nombre: Juan Camilo Neira Campos
+ Código: 201922746
+
+ Nombre: Juan David Briceño Morales
+ Código: 201812887
+ """
 
 
 
+
+from pkg_resources import compatible_platforms
 import collections
+import sys
 
+def alfabeto_pandora(palabras):
+    grafo= construir_grafo(palabras)
+    try:
+        return orden_topologico(grafo)
+    except:
+        return print("ERROR")
+    
 
-def build_graph(words):
+def construir_grafo(words):
     g = {c: [] for w in words for c in w}
+
+    for i in range(1, len(words)):
+        for c1, c2 in zip(words[i-1], words[i]):
+            if c1 != c2:
+                g[c1].append(c2)
+                break
+
     for word in words:
         if len(word)>1:
             # for letter in range(len(word) - 1):
@@ -17,17 +43,11 @@ def build_graph(words):
             #         g[word[0]].append(word[letter+1])
             
             lista = g.get(word[1])
-            print (lista)
+            #print (lista)
             if word[0] != word[1] and word[0] not in lista:
                 g[word[0]].append(word[1])
             
-            
-    for i in range(1, len(words)):
-        for c1, c2 in zip(words[i-1], words[i]):
-            if c1 != c2:
-                g[c1].append(c2)
-                break
-    print (g)
+    #print (g)
     return g
 
 def find_first_letter(g):
@@ -39,10 +59,11 @@ def find_first_letter(g):
     if len(first_letter) != 1:
         raise Exception("no first letter %s" % first_letter)
     return first_letter[0]
+    #return min(g.keys())
 
 def explore(g, c, visited, result):
     if visited[c] == 1:
-        raise Exception("ERROR")
+         raise Exception("ERROR")
     if visited[c] == 2:
         return
     visited[c] = 1
@@ -51,23 +72,48 @@ def explore(g, c, visited, result):
     result.append(c)
     visited[c] = 2
 
-def topo_sort(g):
+def orden_topologico(g):
     visited = collections.defaultdict(lambda: 0)
+    #visited = {}
+    #visited = g
     result = []
     first_letter = find_first_letter(g)
     explore(g, first_letter, visited, result)
     result.reverse()
     return "".join(result)
 
-def sort_alphabet(words):
-    return topo_sort(build_graph(words))
 
 
-if __name__ == "__main__":
-    words =["ab", "ac", "cc", "cb"]
-    print(sort_alphabet(words))
 
+# if __name__ == "__main__":
+#     palabras =["ab", "ac", "cc", "cb"]
+#     alfabeto_pandora(palabras)
+
+#["ab", "ac", "cc", "cb"]
 #["ab", "ah", "an", "mn", "mk"]
 # ["xx", "xp", "pj", "jjj", "jjw"]
-#["h", "hjh", "hjmh", "m", "mh", "mmhj","hm", "j", "jjm"]
+#["h", "hjh", "hjmh", "hm", "j", "jjm",  "m", "mh", "mmhj"]
+
+#["arco", "barca", "cora", "ro"]
     
+
+    
+if __name__ == '__main__':
+    numero_casos = int(sys.stdin.readline())
+    for i in range(numero_casos):
+        n_paginas, n_palabras = sys.stdin.readline().split()
+        n_paginas = int(n_paginas)
+        n_palabras = int(n_palabras)
+        j=0
+        palabras = []
+        dict ={}
+        for pagina in range(n_paginas):
+            lista = (sys.stdin.readline().split())
+            dict[int(lista[0])] = lista[1:]
+        while j<n_paginas:
+            cadena = dict[j]
+            for palabra in cadena:
+                palabras.append(palabra)
+            j+=1
+        print(alfabeto_pandora(palabras))
+
